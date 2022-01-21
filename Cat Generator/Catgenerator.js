c7279549634241dfcc68a929a7b4e54a565a3a32
Rugved76 <89897353+Rugved76@users.noreply.github.com>
2022-01-21 11:32:48 +0530

    Add files via upload
    Generates Cat GIFs

---
+++ Cat Generator/Catgenerator.js
@@ -0,0 +1,7 @@
+function generatecat()
+{
+    var image = document.createElement('img');
+    var div = document.getElementById('flexcatgen');
+    image.src="http://thecatapi.com/api/images/get?format=src&type=gif"
+    div.appendChild(image);
+}
