fc4af0ea156e40ac88cc1cfdefe796acfad0ae63
rugvedwagh <rugvedwagh02@gmail.com>
2023-05-25 16:04:20 +0530

    added css to table

--- musicdb/app.js
+++ musicdb/app.js
@@ -1,21 +1,21 @@
 const express = require('express');
-const bodyParser = require('body-parser');var app = express()
-var port = 5000
-const Music =require('./models')
+const bodyParser = require('body-parser'); var app = express()
+var PORT = 3000
+const Music = require('./models')
+const dbConfig = require('./config');
+const mongoose = require('mongoose');
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-const dbConfig = require('./config');
-const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
 mongoose.connect(dbConfig.url, {
     useNewUrlParser: true
 }).then(() => {
-    console.log("Successfully connected to the database");    
+    console.log("Successfully connected to the database");
 }).catch(err => {
     console.log('Could not connect to the database. Exiting now...', err);
     process.exit();
@@ -29,35 +29,39 @@ app.get('/', (req, res) => {
 app.post("/addsong", (req, res) => {
     var myData = new Music(req.body);
     myData.save()
-    .then(item => {
-    res.send("item saved to database");
-    })
-    .catch(err => {
-    res.status(400).send("unable to save to database");
-    })
+        .then(item => {
+            // res.send("item saved to database");
+            res.redirect('/')
+        })
+        .catch(err => {
+            res.status(400).send("unable to save to database");
+        })
 });
 
 
-app.get('/getSongs',(req,res)=>{
+app.get('/getSongs', (req, res) => {
     console.log(req.query)
     Music.find(req.query).
-    then(music=>{
-        res.render("table",{music:music})
-    }).catch(err=>{
-        res.json({ "message" : err})
-    })
+        then(music => {
+            res.render("table", { music: music })
+        }).catch(err => {
+            res.json({ "message": err })
+        })
 })
 
 
 
-app.post('/deleteSongs/:id',(req,res)=>{
+app.post('/deleteSongs/:id', (req, res) => {
     Music.findByIdAndDelete(req.params.id).
-    then(music=>{
-        console.log("Deleted Successfully")
-        res.redirect('/getSongs')
-    })
+        then(music => {
+            // console.log("Deleted Successfully")
+            res.redirect('/getSongs')
+        }).catch((err)=>{
+            res.json({"message":err})
+        })
 })
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("Server is listening on port 3000");
 });
+
